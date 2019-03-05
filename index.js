6dabb894d56f889fe3abfc72a9b51bcefb0d074c
Arturo Amador Paulino <amadorassassin@gmail.com>
2019-03-05 17:02:25 -0600

    update

--- index.js
+++ index.js
@@ -22,43 +22,84 @@ function insertarAlumnos(nombre, matricula, carrera, fechaNacimiento, pasaTiempo
 }
 
 function consultarAlumnos() {
+    var data = {}
     db.each("SELECT * FROM Main", (err, row) => {
-        var data = {
-            "nombre":row.Nombre,
-            "matricula":row.Matricula,
-            "carrera":row.Carrera,
-            "fechaNacimiento":row.FechaNacimiento,
-            "pasaTiempo":row.Pasatiempo,
-            "calificacion":row.CalificacionEsperada
-        }
+        data = {
+                "nombre":row.Nombre,
+                "matricula":row.Matricula,
+                "carrera":row.Carrera,
+                "fechaNacimiento":row.FechaNacimiento,
+                "pasaTiempo":row.Pasatiempo,
+                "calificacion":row.CalificacionEsperada
+            }
 
         if (err) {
             console.error(err)
             return null
         }else{
+            console.log(data);
             return data
         }
 
     });
+
+    setTimeout(() => {
+        return data;
+    }, 1500);
 }
 
 app.get('/alumnos', (req, res) => {
-    console.log(consultarAlumnos());
-    var data = consultarAlumnos(); 
 
-    console.log(data);
+    var data = {}
+    db.each("SELECT * FROM Main", (err, row) => {
+        data = {
+                "nombre":row.Nombre,
+                "matricula":row.Matricula,
+                "carrera":row.Carrera,
+                "fechaNacimiento":row.FechaNacimiento,
+                "pasaTiempo":row.Pasatiempo,
+                "calificacion":row.CalificacionEsperada
+            }
+        if (err) {
+            console.error(err)
+            res.status(501).send({status:true, data:data});
+            return;
+        }
 
+    });
     setTimeout(() => {
+        console.log(data);
         res.send({status:true, data:data});
-        return;    
-    }, 1000);
+        return;
+        
+    }, 5000);
     
 });
+
+app.post('/registroAlumno',(req,res)=>{
+
+    var nombre = req.body.nombre
+    var matricula = req.body.matricula
+    var carrera = req.body.carrera
+    var fechaNacimiento = req.body.fechaNacimiento
+    var pasaTiempo = req.body.pasaTiempo
+    var calificacionEsperada = req.body.calificacionEsperada
+
+
+    insertarAlumnos(nombre, matricula, carrera, fechaNacimiento, pasaTiempo, calificacionEsperada)
+
+    setTimeout(() => {
+        res.send({status:true});
+        return;
+    }, 5000);
+    
+})
 
 
 
 
 //insertarAlumnos("Arturo Amador",01374416,"ISC",28,"Dormir","100");
+console.log(consultarAlumnos());
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Escuchando en puerto ${port}...`));
