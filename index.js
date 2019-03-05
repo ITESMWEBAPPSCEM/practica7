ae7619921bf86b1185bc66795d5762f42131a119
Arturo Amador Paulino <amadorassassin@gmail.com>
2019-03-04 18:29:47 -0600

    update node modules

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const express = require('express');
+const app = express();
+const Joi = require('joi');
+const path = require('path');
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('practica7.db');
+
+
+app.use("/public", express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+app.use(express.json());
+
 
 function insertarAlumnos(nombre, matricula, carrera, fechaNacimiento, pasaTiempo, calificacionEsperada) {
     db.serialize(params => {
@@ -12,11 +23,42 @@ function insertarAlumnos(nombre, matricula, carrera, fechaNacimiento, pasaTiempo
 
 function consultarAlumnos() {
     db.each("SELECT * FROM Main", (err, row) => {
-        
-        err ? console.error(err): console.log(`${row.Matricula} - ${row.Nombre} - ${row.Carrera}`);
-        
+        var data = {
+            "nombre":row.Nombre,
+            "matricula":row.Matricula,
+            "carrera":row.Carrera,
+            "fechaNacimiento":row.FechaNacimiento,
+            "pasaTiempo":row.Pasatiempo,
+            "calificacion":row.CalificacionEsperada
+        }
+
+        if (err) {
+            console.error(err)
+            return null
+        }else{
+            return data
+        }
+
     });
 }
 
+app.get('/alumnos', (req, res) => {
+    console.log(consultarAlumnos());
+    var data = consultarAlumnos(); 
+
+    console.log(data);
+
+    setTimeout(() => {
+        res.send({status:true, data:data});
+        return;    
+    }, 1000);
+    
+});
+
+
+
+
 //insertarAlumnos("Arturo Amador",01374416,"ISC",28,"Dormir","100");
-consultarAlumnos();
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Escuchando en puerto ${port}...`));
